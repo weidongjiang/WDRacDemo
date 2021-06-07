a3e6c890cbf0f3e56805ded0be7cf4337cb17078
wheatear <wheatear.jiang@qiffa.com>
2021-06-07 15:00:26 +0800

    简单化

--- WDRacDemo/WDRacDemo/ViewController.m
+++ WDRacDemo/WDRacDemo/ViewController.m
@@ -22,51 +22,21 @@
     self.topItemBackView.backgroundColor = [UIColor redColor];
     self.topItemBackView.frame = CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 500);
     
-    [self initSubViews];
+    [self setShareTopViewData];
 }
 
-- (void)initSubViews {
-    UIButton *btn_1 = [[UIButton alloc] init];
-    [btn_1 setTitle:@"btn_1" forState:UIControlStateNormal];
-    
-    UIButton *btn_2 = [[UIButton alloc] init];
-    [btn_2 setTitle:@"btn_2" forState:UIControlStateNormal];
-    
-    UIButton *btn_3 = [[UIButton alloc] init];
-    [btn_3 setTitle:@"btn_3" forState:UIControlStateNormal];
-    
-    UIButton *btn_4 = [[UIButton alloc] init];
-    [btn_4 setTitle:@"btn_4" forState:UIControlStateNormal];
-    
-    UIButton *btn_5 = [[UIButton alloc] init];
-    [btn_5 setTitle:@"btn_5" forState:UIControlStateNormal];
-    
-    UIButton *btn_6 = [[UIButton alloc] init];
-    [btn_6 setTitle:@"btn_6" forState:UIControlStateNormal];
-    
-    NSMutableArray *array = [[NSMutableArray alloc] init];
-    [array addObject:btn_1];
-    [array addObject:btn_2];
-    [array addObject:btn_3];
-    [array addObject:btn_4];
-    [array addObject:btn_5];
-    [array addObject:btn_6];
-    
-    [self setShareTopViewData:[array copy]];
-
-}
-
-- (void)setShareTopViewData:(NSArray *)topViewDataArray {
+- (void)setShareTopViewData {
     
     NSInteger listMaxCount = 5;
     CGFloat pluginVerticalMaign = 30;
     CGFloat pluginHeight = 60;
     CGFloat pluginWidth = 60;
 
-    for (int i = 0; i < topViewDataArray.count; i++) {
-        UIButton *plugin = [topViewDataArray objectAtIndex:i];
+    for (int i = 0; i < 8; i++) {
+        UIButton *plugin = [[UIButton alloc] init];
+        [plugin setTitle:[NSString stringWithFormat:@"btn_%d",i] forState:UIControlStateNormal];
         [plugin addTarget:self action:@selector(pluginDid:) forControlEvents:UIControlEventTouchUpInside];
-
+        [plugin setBackgroundColor:[UIColor blueColor]];
         plugin.tag = i;
         [self.topItemBackView addSubview:plugin];
         NSInteger list_i =  i % listMaxCount; // 列
